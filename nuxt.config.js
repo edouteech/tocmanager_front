d8365d2c72b7360024f47acbeb39ac4cc90eaefd
edouteech <55913793+edouteech@users.noreply.github.com>
2022-08-18 07:14:58 +0100

    mise à jour

--- nuxt.config.js
+++ nuxt.config.js
@@ -86,7 +86,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: "http://localhost:8000/api",
+    baseURL: "http://api.tech-plux.com/api",
     credentials:true
   },
 
