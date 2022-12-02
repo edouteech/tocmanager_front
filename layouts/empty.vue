1c5639cdb1c1ec86b2aa05660978c55db81caca1
Sostom <chrystokou07@gmail.com>
2022-12-02 15:41:59 +0100

    02/12/2022

--- layouts/empty.vue
+++ layouts/empty.vue
@@ -1,6 +1,9 @@
 <template>
 <div>
     <div class="contenu">
+        <div class="alert alert-danger justify-content-center avertissement-abonnement" role="alert" v-if="verifyGrace != null">
+          Votre abonnement est expiré !!! Vous bénéficiez d'une période de grace. Veuillez vous réabonnez avant le {{verifyGrace}}.
+        </div>
         <Nuxt />
     </div>
     <Footer/>
@@ -15,6 +18,19 @@ export default{
     name: 'empty',
     components:{
         Footer,
+    },
+    data(){
+        return {
+            verifyGrace : ''
+        }
+    },
+
+    mounted(){
+        this.$axios.get('/compagnies/grace/'+localStorage.getItem('auth.company_id'))
+        .then(response =>{
+            console.log(response)
+            this.verifyGrace = response.data.data.graceEndDate
+        })
     }
 } 
 
