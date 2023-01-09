fa5b47b555f6c577ccb48efc4ccbe1dcdb7036f3
edouteech <55913793+edouteech@users.noreply.github.com>
2023-01-09 14:44:41 +0100

    tt

--- nuxt.config.js
+++ nuxt.config.js
@@ -97,7 +97,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: "https://teste.tocmanager.com/api",
+    baseURL: "https://api.tocmanager.com/api",
     credentials:true
   },
 
