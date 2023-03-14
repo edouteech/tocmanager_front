8af9c417d895adc0287d9fb64913e21870897f28
Sostom <chrystokou07@gmail.com>
2023-03-14 11:16:36 +0100

    indenter, commentaires et corrections dans clients

--- pages/clients/_id.vue
+++ pages/clients/_id.vue
@@ -1,204 +1,256 @@
 <template>
-<div>
-    <nav class="navbar navbar-fixed-top navbar-dark bg-dark text-white p-3"> 
-      <Sidebar /><h3 class="name_side">Clients </h3>
+  <div>
+    <nav class="navbar navbar-fixed-top navbar-dark bg-dark text-white p-3">
+      <Sidebar />
+      <h3 class="name_side">Clients</h3>
       <Userinfo />
     </nav>
 
     <div class="app-main__outer py-5 px-2">
-        <div class="alert alert-danger justify-content-center" role="alert" v-if="error">
-            {{error}}
+      <div
+        class="alert alert-danger justify-content-center"
+        role="alert"
+        v-if="error"
+      >
+        {{ error }}
+      </div>
+
+      <h4>Modifier les informations de ce client</h4>
+      <hr />
+      <form action="">
+        <div class="form-group col-md-6">
+          <label class="title">Entrer le nom du client</label>
+          <input
+            type="text"
+            class="form-control"
+            v-model="form.name"
+            autocomplete="off"
+            required
+            placeholder="Jean Doe"
+          />
         </div>
-        <h4>Modifier les informations de ce client</h4><hr>
-        <form action="">
-            <div class="form-group col-md-6 ">
-                <label class="title">Entrer le nom du client</label>
-                <input type="text" class="form-control" v-model="form.name" autocomplete="off" required placeholder="Jean Doe">
-            </div>
-            <div class="alert alert-danger justify-content-center" role="alert" v-if="errors && errors.name">
-                {{errors.name}}
-            </div>
+        <div
+          class="alert alert-danger justify-content-center"
+          role="alert"
+          v-if="errors && errors.name"
+        >
+          {{ errors.name }}
+        </div>
 
-            <div class="form-group col-md-6 ">
-                <label class="title">Entrer le numero de téléphone du client</label>
-                <input type="tel" class="form-control" v-model="form.phone" required  placeholder="+525485335622">
-            </div>
-            <div class="alert alert-danger justify-content-center" role="alert" v-if="errors && errors.phone">
-                {{errors.phone}}
-            </div>
+        <div class="form-group col-md-6">
+          <label class="title">Entrer le numero de téléphone du client</label>
+          <input
+            type="tel"
+            class="form-control"
+            v-model="form.phone"
+            required
+            placeholder="+525485335622"
+          />
+        </div>
+        <div
+          class="alert alert-danger justify-content-center"
+          role="alert"
+          v-if="errors && errors.phone"
+        >
+          {{ errors.phone }}
+        </div>
 
-            <div class="form-group col-md-6">
-                <label class="title">Entrer l'email du client</label>
-                <input type="email" class="form-control" v-model="form.email" autocomplete="off" required  placeholder="azerty@azert.com" >
-            </div>
-            <div class="alert alert-danger justify-content-center" role="alert" v-if="errors && errors.email">
-                {{errors.email}}
-            </div>
+        <div class="form-group col-md-6">
+          <label class="title">Entrer l'email du client</label>
+          <input
+            type="email"
+            class="form-control"
+            v-model="form.email"
+            autocomplete="off"
+            required
+            placeholder="azerty@azert.com"
+          />
+        </div>
+        <div
+          class="alert alert-danger justify-content-center"
+          role="alert"
+          v-if="errors && errors.email"
+        >
+          {{ errors.email }}
+        </div>
 
-            <div class="form-group col-md-6">
-                <div class="form-group ">
-                <label class="title">Nature du client</label>
-                <select class="form-control" v-model="form.nature">
-                        <option disabled value="">Choisissez...</option>
-                        <option value="0">Particulier</option>
-                        <option value="1">Entreprise</option>
-                </select>
-                </div>
-            </div>
-            <div class="alert alert-danger justify-content-center" role="alert" v-if="errors && errors.nature">
-                {{errors.nature}}
-            </div>
-            
-            <div class="form-group col-md-6" v-if="role == 'admin'">
-                <label class="title">Type de client</label>
-                <select class="form-control" v-model="form.type_client" >
-                        <option disabled value="">Choisissez...</option>
-                        <option :value="type" v-for="(type, i) in types" :key="i">{{type}}</option>
-                </select>
-            </div>
-            <div class="form-group col-md-6" v-if="form.type_client == 'douteux'">
-                <label class="title">Entrer le seuil de crédit possible</label>
-                <input type="email" class="form-control" v-model="form.seuil_max" autocomplete="off" required  placeholder="azerty@azert.com" >
-            </div>
+        <div class="form-group col-md-6">
+          <div class="form-group">
+            <label class="title">Nature du client</label>
+            <select class="form-control" v-model="form.nature">
+              <option disabled value="">Choisissez...</option>
+              <option value="0">Particulier</option>
+              <option value="1">Entreprise</option>
+            </select>
+          </div>
+        </div>
+        <div
+          class="alert alert-danger justify-content-center"
+          role="alert"
+          v-if="errors && errors.nature"
+        >
+          {{ errors.nature }}
+        </div>
 
-            <button type="submit" class="btn btn-primary" v-on:click.prevent="submit()">Enregistrer le client</button>
-        </form>
+        <div class="form-group col-md-6" v-if="role == 'admin'">
+          <label class="title">Type de client</label>
+          <select class="form-control" v-model="form.type_client">
+            <option disabled value="">Choisissez...</option>
+            <option :value="type" v-for="(type, i) in types" :key="i">
+              {{ type }}
+            </option>
+          </select>
+        </div>
+        <div class="form-group col-md-6" v-if="form.type_client == 'douteux'">
+          <label class="title">Entrer le seuil de crédit possible</label>
+          <input
+            type="email"
+            class="form-control"
+            v-model="form.seuil_max"
+            autocomplete="off"
+            required
+            placeholder="azerty@azert.com"
+          />
+        </div>
+
+        <button
+          type="submit"
+          class="btn btn-primary"
+          v-on:click.prevent="submit()"
+        >
+          Enregistrer le client
+        </button>
+      </form>
     </div>
-      
-</div>
-
+  </div>
 </template>
 
 <script>
-import Sidebar from '../sidebar.vue'
-import Userinfo from '../user_info.vue'
+import Sidebar from "../sidebar.vue";
+import Userinfo from "../user_info.vue";
 export default {
-    layout: "empty",
-    auth:true,
-    components: {
-        Sidebar,  
-        Userinfo
+  layout: "empty",
+  auth: true,
+  components: {
+    Sidebar,
+    Userinfo,
+  },
+  data() {
+    return {
+      client: "",
+      clients: [],
+      form: {
+        id: "",
+        name: "",
+        email: "",
+        phone: "",
+        nature: "",
+        type_client: "",
+        compagnie_id: "",
+      },
+      error: null,
+      errors: [],
+      types: "",
+      role: "",
+    };
+  },
+  mounted() {
+    this.recupClient();
+    this.recupType();
+    this.role = localStorage.getItem("auth.roles");
+  },
+
+  methods: {
+    //recupérer les informations du client concerné
+    recupClient() {
+      this.$axios
+        .get("/clients/" + this.$route.params.id, {
+          params: {
+            compagnie_id: localStorage.getItem("auth.company_id"),
+          },
+        })
+        .then((response) => {
+          let client = response.data.data[0];
+          (this.form.name = client.name),
+            (this.form.phone = client.phone),
+            (this.form.email = client.email);
+          this.form.seuil_max = client.seuil_max;
+          this.form.type_client = client.type_client;
+          if (client.nature == "Particulier") {
+            this.form.nature = 0;
+          } else {
+            this.form.nature = 1;
+          }
+        });
     },
-    data () {
-        return{
-            client: "",
-            clients: [],
-            form: {
-                id: '',
-                name: '',
-                email: '',
-                phone: '',
-                nature:'',
-                type_client: '',
-                compagnie_id: '',
-            },
-            error:null,
-            errors: [],
-            types: '',
-            role: ''
-        }
-        },
-    mounted() {
-        this.recupClient()
-        this.recupType()  
-        this.role = localStorage.getItem('auth.roles')   
+
+    //recupérer les types de client
+    recupType() {
+      this.$axios
+        .get("/clients/types", {
+          params: {
+            compagnie_id: localStorage.getItem("auth.company_id"),
+          },
+        })
+        .then((response) => {
+          this.types = response.data.data;
+        });
     },
 
-    methods: {
-
-        recupClient(){
-            this.$axios.get('/clients/'+ this.$route.params.id,{
-                params: {
-                compagnie_id: localStorage.getItem('auth.company_id')
-                }
-            })
-            .then(response => {
-                // console.log(response.data.data[0] )
-                let client = response.data.data[0];
-                // this.clients = response.data.data
-                this.form.name = client.name,
-                this.form.phone = client.phone,
-                this.form.email = client.email
-                this.form.seuil_max = client.seuil_max
-                this.form.type_client = client.type_client
-                if(client.nature == "Particulier"){
-                    this.form.nature = 0
-                }else{
-                    this.form.nature = 1
-                }
-                
-            })
-
-        },
-
-        recupType(){
-            this.$axios.get('/clients/types',{
-                params: {
-                compagnie_id: localStorage.getItem('auth.company_id'),
-                }
-            }).then(response =>{
-                // console.log(response);
-                this.types = response.data.data
-                })   
-        },
-
-        submit(){          
-            this.$axios.put('/clients/' +this.$route.params.id,{
-                id: this.$route.params.id,
-                name: this.form.name,
-                email: this.form.email,
-                phone: this.form.phone,
-                nature: this.form.nature,
-                type_client: this.form.type_client,
-                seuil_max: this.form.seuil_max,
-                compagnie_id: localStorage.getItem('auth.company_id')
-
-            })
-            .then(response =>{
-                if(response.data.status == "success"){
-                    this.$router.push({path:'/clients/list_client',})
-                    this.$toast('Modifications éffectuées avec succès !!!', {
-                        icon: 'fa fa-check-circle',
-                    })
-                }
-                else{
-                    this.error = response.data.message
-                    this.errors = response.data.data
-                    // this.$router.push({path:'/clients/add_client'});
-                }
-            })          
-        }
-    }
-   
-}
+    //modification des infos du client
+    submit() {
+      this.$axios
+        .put("/clients/" + this.$route.params.id, {
+          id: this.$route.params.id,
+          name: this.form.name,
+          email: this.form.email,
+          phone: this.form.phone,
+          nature: this.form.nature,
+          type_client: this.form.type_client,
+          seuil_max: this.form.seuil_max,
+          compagnie_id: localStorage.getItem("auth.company_id"),
+        })
+        .then((response) => {
+          if (response.data.status == "success") {
+            this.$router.push({ path: "/clients/list_client" });
+            this.$toast("Modifications éffectuées avec succès !!!", {
+              icon: "fa fa-check-circle",
+            });
+          } else {
+            this.error = response.data.message;
+            this.errors = response.data.data;
+          }
+        });
+    },
+  },
+};
 </script>
 
 <style scoped>
-
-form{
-    margin-left: 10%;
-    margin-top: 5%;
+form {
+  margin-left: 10%;
+  margin-top: 5%;
 }
 
-.form-group{
-    margin-top: 2%;
+.form-group {
+  margin-top: 2%;
 }
 
-.title{
-    margin: 1% 0;
+.title {
+  margin: 1% 0;
 }
 
-.btn{
-    margin-top: 5%;
+.btn {
+  margin-top: 5%;
 }
 
-.app-main__outer{
+.app-main__outer {
   overflow: auto;
 }
 
 input {
-    border: none; outline: none;
-    border-bottom: 2px solid #605050;
+  border: none;
+  outline: none;
+  border-bottom: 2px solid #605050;
 }
-       
 </style>
--- pages/clients/modal_delete.vue
+++ pages/clients/modal_delete.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="modal-overlay" @click="$emit('close-modal')">
-    <div class="modaler" @click.stop>                     
-          <p>Etes vous sur de vouloir supprimer définitivement ce client ??? </p>
-          <div class="reponse">
-                <div class="yes"  @click="supClient(infos)">
-                    Oui
-                </div>
-                <div class="no" @click="$emit('close-modal')">
-                    Non
-                </div>
-          </div>
+    <div class="modaler">
+      <div
+        class="alert alert-danger justify-content-center"
+        role="alert"
+        v-if="error"
+      >
+        {{ error }}
+      </div>
+      <p>Etes vous sur de vouloir supprimer définitivement ce client ???</p>
+      <div class="reponse">
+        <div class="yes" @click="supClient(infos)">Oui</div>
+        <div class="no" @click="$emit('close-modal')">Non</div>
+      </div>
     </div>
     <div class="close" @click="$emit('close-modal')">
       <img class="close-img" src="/images/fermer.png" alt="" />
@@ -18,73 +21,72 @@
 </template>
 
 <script>
-  export default {
-    name: 'deleteModal',
-    props: ['infos'],
-    data () {
-        return {
-            client: "",
-        }
-    }, 
-    methods: {
-        supClient(infos){
-            console.log(infos);
-            this.$axios.delete('/clients/' +infos,{
-            params: {
-              compagnie_id: localStorage.getItem('auth.company_id')
-            }})           
-            .then(response => {
-              // console.log(response.data);
-                  this.client = response.data.data;
-                   this.$router.push({path:'/corbeille',})})                               
-        },
-
-        refresh(){
-          this.$axios.get('/get/client')
-          .then(response => {console.log(response);})
-        }
+export default {
+  name: "deleteModal",
+  props: ["infos"],
+  data() {
+    return {
+      client: "",
+      error: null,
+    };
+  },
+  methods: {
+    supClient(infos) {
+      this.$axios
+        .delete("/clients/" + infos, {
+          params: {
+            compagnie_id: localStorage.getItem("auth.company_id"),
+          },
+        })
+        .then((response) => {
+          if (response.data.status == "success") {
+            this.client = response.data.data;
+            this.$router.push({ path: "/corbeille" });
+          } else {
+            this.error = response.data.message;
+          }
+        });
     },
 
-}    
-        
-            
-            
-        
+    refresh() {
+      this.$axios.get("/get/client").then((response) => {
+        console.log(response);
+      });
+    },
+  },
+};
 </script>
 
 <style scoped>
-
-.reponse{
-    display: flex;
-    
+.reponse {
+  display: flex;
 }
 
-.yes{
-    padding: 10px;
-    margin: 5%;
-    text-align: center;
-    border: 1px solid;
-    cursor: pointer;
-    margin-left: 30%;
-    border-radius: 10px;
+.yes {
+  padding: 10px;
+  margin: 5%;
+  text-align: center;
+  border: 1px solid;
+  cursor: pointer;
+  margin-left: 30%;
+  border-radius: 10px;
 }
-.no{
-    padding: 10px;
-    margin: 5%;
-    text-align: center;
-    border: 1px solid;
-    cursor: pointer;
-    border-radius: 10px;
-}
-
-.yes:hover{
-    background-color: rgb(201, 220, 251);
+.no {
+  padding: 10px;
+  margin: 5%;
+  text-align: center;
+  border: 1px solid;
+  cursor: pointer;
+  border-radius: 10px;
 }
 
-.no:hover{
-    background-color: rgb(201, 220, 251);
+.yes:hover {
+  background-color: rgb(201, 220, 251);
 }
 
+.no:hover {
+  background-color: rgb(201, 220, 251);
+}
 
 .modal-overlay {
   position: fixed;
@@ -94,13 +96,13 @@
   right: 0;
   display: flex;
   justify-content: center;
-  background-color: rgba(239, 239, 239, 0.803);;
+  background-color: rgba(239, 239, 239, 0.803);
 }
 
 .modaler {
   text-align: center;
-  background-color: rgb(209, 0, 0);;
-  height: 250px;
+  background-color: rgb(209, 0, 0);
+  height: max-content;
   width: 500px;
   margin-top: 12%;
   border-radius: 15px;
