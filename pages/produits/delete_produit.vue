1e17b5008a71a9d73925b7abed9992d1999c34ef
Sostom <chrystokou07@gmail.com>
2023-03-15 10:11:32 +0100

    indenter, commentaires et corrections dans produit

--- pages/produits/delete_produit.vue
+++ pages/produits/delete_produit.vue
@@ -1,195 +1,262 @@
 <template>
-<div>
-    <nav class="navbar navbar-fixed-top navbar-dark bg-dark text-white p-3"> 
-      <Sidebar /><h3 class="name_side">Produits </h3>
+  <div>
+    <nav class="navbar navbar-fixed-top navbar-dark bg-dark text-white p-3">
+      <Sidebar />
+      <h3 class="name_side">Produits</h3>
       <User_info />
     </nav>
 
     <div class="app-main__outer py-5 px-2">
-      <h4>Produits supprimés</h4><hr>
-        <div class="table-responsive">
-            <table class="table table-hover">
-            <thead>
-                <tr class="table-success">
-                            <th >
-                                Noms 
-                            </th>
-                            <th >
-                                Quantités
-                            </th>
-                            <th>
-                                Prix de vente
-                            </th>
-                            <th>
-                                Prix d'achat
-                            </th>
+      <h4>Produits supprimés</h4>
+      <hr />
+      <div
+        class="alert alert-danger justify-content-center"
+        role="alert"
+        v-if="error"
+      >
+        {{ error }}
+      </div>
+      <div class="table-responsive">
+        <table class="table table-hover">
+          <thead>
+            <tr class="table-success">
+              <th>Noms</th>
+              <th>Quantités</th>
+              <th>Prix de vente</th>
+              <th>Prix d'achat</th>
 
-                            <th>
-                                Actions
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr  v-for="(produit, i) in produits" :key="i"
-                            class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                            <td>{{produit.name}}</td>
-                            <td>{{produit.quantity}}</td>
-                            <td>{{produit.price_sell}}</td>
-                            <td>{{produit.price_buy}}</td>                         
-                            <td><div class="action d-flex aligns-items-center justify-content-center">
-                                <div class="sup" @click="supProduit(produit.id)">Supprimer définitivement</div>
-                                <div class="restore" @click="restaurerProduit(produit.id)">Restaurer ce produit</div></div>
-                            </td>
-                        </tr>
-                        
-                    </tbody>
-            </table>
-            <p class="text-center"><strong>{{total}} produits au total </strong></p><hr class="text-primary">
-        </div><br><br> 
-            
-        <form action="">
-            <div class="nombre d-flex my-4 col-md-2">
-                <label class="title mx-3 my-2"><strong> Affichage:</strong></label> 
-                <select class="form-control " v-model="form.nombre" required @click.prevent="refresh()">
-                    <option value="10">10</option>
-                    <option value="25" >25</option>
-                    <option value="50">50</option>
-                    <option value="100">100</option>
-                </select>
-            </div>
-        </form>
-        <nav aria-label="Page navigation example "  class="d-flex nav" v-if="res_data != null">
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="(produit, i) in produits"
+              :key="i"
+              class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+            >
+              <td>{{ produit.name }}</td>
+              <td>{{ produit.quantity }}</td>
+              <td>{{ produit.price_sell }}</td>
+              <td>{{ produit.price_buy }}</td>
+              <td>
+                <div
+                  class="action d-flex aligns-items-center justify-content-center"
+                >
+                  <div class="sup" @click="supProduit(produit.id)">
+                    Supprimer définitivement
+                  </div>
+                  <div class="restore" @click="restaurerProduit(produit.id)">
+                    Restaurer ce produit
+                  </div>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <p class="text-center">
+          <strong>{{ total }} produits au total </strong>
+        </p>
+        <hr class="text-primary" />
+      </div>
+      <br /><br />
+
+      <form action="">
+        <div class="nombre d-flex my-4 col-md-2">
+          <label class="title mx-3 my-2"><strong> Affichage:</strong></label>
+          <select
+            class="form-control"
+            v-model="form.nombre"
+            required
+            @click.prevent="refresh()"
+          >
+            <option value="10">10</option>
+            <option value="25">25</option>
+            <option value="50">50</option>
+            <option value="100">100</option>
+          </select>
+        </div>
+      </form>
+      <nav
+        aria-label="Page navigation example "
+        class="d-flex nav"
+        v-if="res_data != null"
+      >
         <ul class="pagination">
-            <li :class="(res_data.prev_page_url == null)? 'page-item disabled':'page-item'"><a class="page-link" @click="refresh(res_data.current_page - 1)">Précédent</a></li>
-            <li class="page-item" v-for="(link, index) in res_data.links" :key="index"><a :class="(link.active == true)? 'page-link active':'page-link'" href="#" @click="refresh(link.label)">{{link.label}}</a></li>
-            
-            <li :class="(res_data.next_page_url == null)? 'page-item disabled':'page-item'"><a class="page-link" @click="refresh(res_data.current_page + 1)">Suivant</a></li>
+          <li
+            :class="
+              res_data.prev_page_url == null
+                ? 'page-item disabled'
+                : 'page-item'
+            "
+          >
+            <a class="page-link" @click="refresh(res_data.current_page - 1)"
+              >Précédent</a
+            >
+          </li>
+          <li
+            class="page-item"
+            v-for="(link, index) in res_data.links"
+            :key="index"
+          >
+            <a
+              :class="link.active == true ? 'page-link active' : 'page-link'"
+              href="#"
+              @click="refresh(link.label)"
+              >{{ link.label }}</a
+            >
+          </li>
+
+          <li
+            :class="
+              res_data.next_page_url == null
+                ? 'page-item disabled'
+                : 'page-item'
+            "
+          >
+            <a class="page-link" @click="refresh(res_data.current_page + 1)"
+              >Suivant</a
+            >
+          </li>
         </ul>
-        </nav>
-    </div><br>
-<deleteModal :infos= 'identifiant' v-show="showModal" @close-modal="showModal = false"/>
-
-
-</div>
+      </nav>
+    </div>
+    <br />
+    <deleteModal
+      :infos="identifiant"
+      v-show="showModal"
+      @close-modal="showModal = false"
+    />
+  </div>
 </template>
 
 
 <script>
-import deleteModal from './modal_delete.vue'
-import Sidebar from '../sidebar.vue'
-import User_info from '../user_info.vue'
+import deleteModal from "./modal_delete.vue";
+import Sidebar from "../sidebar.vue";
+import User_info from "../user_info.vue";
 export default {
-    layout: "empty",
-    components: {
+  layout: "empty",
+  components: {
     Sidebar,
     deleteModal,
-    User_info
-},
+    User_info,
+  },
 
-    data () {
-        return {
-            links: [],
-            res_data: null,
-            showModal: false,
-            produit: "",
-            identifiant : "0",
-            produits: [],
-            total: '',
-            form: {
-                nombre: '',
-            }
-        }
-    },   
+  data() {
+    return {
+      error: null,
+      links: [],
+      res_data: null,
+      showModal: false,
+      produit: "",
+      identifiant: "0",
+      produits: [],
+      total: "",
+      form: {
+        nombre: "",
+      },
+    };
+  },
 
-    mounted () {
-         this.$axios.get('/get/products',{params: {
-            compagnie_id: localStorage.getItem('auth.company_id'),
-            // page: page,
-            per_page : this.form.nombre }   
-          })        
-        .then(response => {console.log(response.data.data);
-            this.produits = response.data.data.data
-            this.res_data= response.data.data 
-            this.total = response.data.data.total
-            let firstE = response.data.data.links.shift()
-            let lastE = response.data.data.links.splice(-1,1);})        
+  mounted() {
+    //liste des produits supprimés
+    this.$axios
+      .get("/get/products", {
+        params: {
+          compagnie_id: localStorage.getItem("auth.company_id"),
+          per_page: this.form.nombre,
+        },
+      })
+      .then((response) => {
+        this.produits = response.data.data.data;
+        this.res_data = response.data.data;
+        this.total = response.data.data.total;
+        let firstE = response.data.data.links.shift();
+        let lastE = response.data.data.links.splice(-1, 1);
+      });
+  },
+
+  methods: {
+    //restauration du produit
+    restaurerProduit(id) {
+      this.$axios
+        .get("/restore/product/" + id, {
+          params: {
+            compagnie_id: localStorage.getItem("auth.company_id"),
+          },
+        })
+        .then((response) => {
+          if (response.data.status == "success") {
+            this.$toast("Suppression !!!", {
+              icon: "fa fa-check-circle",
+            });
+            this.produit = response.data.data;
+            this.$router.push({ path: "/produits/list_produit" });
+          } else {
+            this.error = response.data.message;
+          }
+        });
     },
 
-    methods: {
-        restaurerProduit(id){
-            console.log(id);
-            this.$axios.get('/restore/product/' +id,{
-            params: {
-              compagnie_id: localStorage.getItem('auth.company_id')
-            }})              
-            .then(response => {console.log(response);
-                this.produit = response.data.data
-                this.$router.push({path:'/produits/list_produit',})
-                })         
-        },
-
-        supProduit(id){
-             console.log(id); 
-            this.showModal = true;
-            this.identifiant= id;               
-        },
+    //modal de suppression
+    supProduit(id) {
+      this.showModal = true;
+      this.identifiant = id;
     },
-  
-}
+  },
+};
 </script>
 
 <style scoped>
-  .nav{
-    overflow: auto;
-  }
-  
-.app-main__outer{
+.nav {
   overflow: auto;
 }
 
-.action{
-    display: flex;
-    margin: 0 15%;
+.app-main__outer {
+  overflow: auto;
 }
-.table{
-	margin-top: 5%;
+
+.action {
+  display: flex;
+  margin: 0 15%;
+}
+.table {
+  margin-top: 5%;
   text-align: center;
-}  
-
-.table th{
-	padding: 20px;
 }
 
-tbody tr:last-of-type{
-    border-bottom: 2px solid rgb(241, 20, 20);
-}   
-.sup{
-    border: 1px solid black;
-    border-radius: 15px;
-    height: 35px;
-    padding: 5px;
-    margin: 0 10px;
-    cursor: pointer;
-    
+.table th {
+  padding: 20px;
 }
 
-.sup:hover{
-    color: #fff;
-    background-color: red;
+tbody tr:last-of-type {
+  border-bottom: 2px solid rgb(241, 20, 20);
+}
+.sup {
+  border: 1px solid black;
+  border-radius: 15px;
+  height: 35px;
+  padding: 5px;
+  margin: 0 10px;
+  cursor: pointer;
 }
 
-.restore{
-    cursor: pointer;
-    border: 1px solid black;
-    border-radius: 15px;
-    height: 35px;
-    padding: 5px;
-    margin: 0 10px;
+.sup:hover {
+  color: #fff;
+  background-color: red;
 }
 
-.restore:hover{
-    color: #fff;
-    background-color: green;
+.restore {
+  cursor: pointer;
+  border: 1px solid black;
+  border-radius: 15px;
+  height: 35px;
+  padding: 5px;
+  margin: 0 10px;
 }
 
+.restore:hover {
+  color: #fff;
+  background-color: green;
+}
 </style>
--- pages/produits/voir_produit.vue
+++ pages/produits/voir_produit.vue
@@ -1,82 +1,55 @@
 <template>
   <div class="modal-overlay" @click="$emit('close-modal')">
-    <div class="modaler" @click.stop>   
-            <h4 class="titre">Informations du produit</h4><br><br>
-              <table class="table table-hover">
-                <thead>
-                  <tr class="table-success">
-                        <th>A savoir</th>
-                        <th>Produit</th>
-                        
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>Catégorie concernée</td>
-                    <td>{{id}}</td>
-                  </tr>
-                  <tr>
-                    <td>Nom du produit</td>
-                    <td>{{nom}}</td>
-                  </tr>
-                  <tr>
-                    <td>Quantité stockée</td>
-                    <td>{{quantite}}</td>
-                  </tr>
-                  <tr>
-                    <td>Prix de vente</td>
-                    <td>{{vente}}</td>
-                  </tr>
-                  <tr>
-                    <td>Prix d'achat</td>
-                    <td>{{achat}}</td>
-                  </tr>
-                  <tr>
-                    <td>Stock minimal</td>
-                    <td>{{min}}</td>
-                  </tr>
-                  <tr>
-                    <td>Stock maximal</td>
-                    <td>{{max}}</td>
-                  </tr>
-                  <tr>
-                    <td>Groupe de taxation</td>
-                    <td>{{group}}</td>
-                  </tr>
-                  <tr>
-                    <td>Code du produit</td>
-                    <td>{{code}}</td>
-                  </tr>
-                  
-                </tbody>
-              </table><br>
-              <!-- <button class="btn btn-outline-dark mx-4" type="submit" @click.prevent="pdf()">Télécharger l'état du stock</button> -->
-            
-                <!-- <div class="input-form">					
-                   <span class="mode">Catégorie concernée : </span> <span class="resp">{{id}}</span>
-                </div>     <br>
-
-                <div class="input-form">					
-                   <span class="mode">Nom du produit : </span> <span class="resp">{{nom}}</span>
-                </div>     <br>
-
-                <div class="input-form">        
-                   <span class="mode">Quantité stockée : </span><span class="resp"> {{quantite}}</span>
-                </div><br>
-            
-                <div class="input-form">    
-                    <span class="mode">Prix de vente : </span><span class="resp">{{vente}}</span>
-                </div><br>
-                <div class="input-form"> 
-                   <span class="mode">Prix d'achat : </span> <span class="resp">{{achat}}</span>
-                </div><br>
-                <div class="input-form">					
-                   <span class="mode">Stock minimal : </span> <span class="resp">{{min}}</span>
-                </div>     <br>
-                <div class="input-form">					
-                   <span class="mode">Stock maximal : </span> <span class="resp">{{max}}</span>
-                </div>     -->
-
+    <div class="modaler">
+      <h4 class="titre">Informations du produit</h4>
+      <br /><br />
+      <table class="table table-hover">
+        <thead>
+          <tr class="table-success">
+            <th>A savoir</th>
+            <th>Produit</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Catégorie concernée</td>
+            <td>{{ id }}</td>
+          </tr>
+          <tr>
+            <td>Nom du produit</td>
+            <td>{{ nom }}</td>
+          </tr>
+          <tr>
+            <td>Quantité stockée</td>
+            <td>{{ quantite }}</td>
+          </tr>
+          <tr>
+            <td>Prix de vente</td>
+            <td>{{ vente }}</td>
+          </tr>
+          <tr>
+            <td>Prix d'achat</td>
+            <td>{{ achat }}</td>
+          </tr>
+          <tr>
+            <td>Stock minimal</td>
+            <td>{{ min }}</td>
+          </tr>
+          <tr>
+            <td>Stock maximal</td>
+            <td>{{ max }}</td>
+          </tr>
+          <tr>
+            <td>Groupe de taxation</td>
+            <td>{{ group }}</td>
+          </tr>
+          <tr>
+            <td>Code du produit</td>
+            <td>{{ code }}</td>
+          </tr>
+        </tbody>
+      </table>
+      <br />
     </div>
     <div class="close" @click="$emit('close-modal')">
       <img class="close-img" src="/images/fermer.png" alt="" />
@@ -85,39 +58,26 @@
 </template>
 
 <script>
-  export default {
-    auth:true,
-    props: ['prod_id','id','nom', 'quantite', 'vente', 'achat', 'min', 'max', 'group', 'code'],
-    name: 'voirProduit',
-
-    methods:{
-
-        pdf() {
-          this.$axios.get('/products/'+this.prod_id+'/download', {
-            params: {
-              compagnie_id: localStorage.getItem('auth.company_id')
-            },
-            responseType: 'blob',
-            Accept: 'application/pdf'
-          }).then((response) => {
-            // console.log(response);
-            const url = window.URL.createObjectURL(new Blob([response.data], {type: 'application/pdf'}));
-            const link = document.createElement('a');
-            link.href = url;
-            link.setAttribute('download', this.nom+'_stock.pdf'); //or any other extension
-            document.body.appendChild(link);
-            link.click();
-          })
-        },
-    }
-    
-}
-
+export default {
+  auth: true,
+  props: [
+    "prod_id",
+    "id",
+    "nom",
+    "quantite",
+    "vente",
+    "achat",
+    "min",
+    "max",
+    "group",
+    "code",
+  ],
+  name: "voirProduit",
+};
 </script>
 
 
 <style scoped>
-
 .modal-overlay {
   position: fixed;
   top: 0;
@@ -130,8 +90,7 @@
   z-index: 99;
 }
 
-
-.titre{
+.titre {
   border: 1px solid black;
   background-color: black;
   color: #ffff;
@@ -139,14 +98,13 @@
   text-align: center;
 }
 
-tbody tr:nth-child(even){
-	background-color: rgb(246, 241, 246);
+tbody tr:nth-child(even) {
+  background-color: rgb(246, 241, 246);
 }
 
-tbody tr:last-of-type{
-	border-bottom: 2px solid rgb(92, 237, 8);
+tbody tr:last-of-type {
+  border-bottom: 2px solid rgb(92, 237, 8);
 }
-
 
 .modaler {
   background-color: white;
@@ -166,25 +124,23 @@ tbody tr:last-of-type{
   width: 25px;
 }
 
-.modal .information{
-    text-align: center;
-    padding-left: 20%;
-    font-weight: bold;
-    font-size: 24px;
-    text-decoration: underline;
-    color: darkblue;
-
+.modal .information {
+  text-align: center;
+  padding-left: 20%;
+  font-weight: bold;
+  font-size: 24px;
+  text-decoration: underline;
+  color: darkblue;
 }
 
-.input-form .mode{
-    font-size: 16px;
-    font-weight: bold;
+.input-form .mode {
+  font-size: 16px;
+  font-weight: bold;
 }
 
-.input-form .resp{
-    font-size: 18px;
-    margin-left: 3%;
-    color: rgb(11, 7, 40);
+.input-form .resp {
+  font-size: 18px;
+  margin-left: 3%;
+  color: rgb(11, 7, 40);
 }
-
 </style>
