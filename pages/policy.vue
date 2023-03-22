48f66e2b10bca566532dd6b50684edf2474fd7c5
edouteech <55913793+edouteech@users.noreply.github.com>
2023-03-22 18:08:09 +0100

    changement de design

---
+++ pages/policy.vue
@@ -0,0 +1,119 @@
+<template>
+    <div>
+        <h1>Mentions légales et politique de confidentialité</h1>
+
+        Nous accordons une grande importance à la protection de vos données personnelles et à la confidentialité de vos informations. Cette politique de confidentialité décrit les pratiques que nous utilisons pour collecter, stocker, utiliser et protéger vos données personnelles.
+
+        Mentions légales
+
+        1.1 Site (ci-après « le site ») :
+
+        https://tocmanager.com
+
+        1.2 Éditeur (ci-après « l'éditeur ») :
+
+        EDOUTECH SARL
+        située : BIDOSSESSI (Abomey Calavi), Rep du Bénin
+        adresse mail : contact@edoutech.com
+
+        1.3 Hébergeur (ci-après « l'hébergeur ») :
+
+        https://tocmanager.com est hébergé par amazon, dont le siège social est situé 43 rue mathias grunewald Strasbourg.
+
+
+
+Collecte de données personnelles 
+
+Nous collectons les données personnelles que vous nous fournissez directement, telles que votre nom, votre adresse e-mail, votre adresse, votre numéro de téléphone et d’autres informations similaires. Nous collectons également des données techniques, telles que l’adresse IP de votre appareil, votre pays, les informations sur votre navigateur et votre système d’exploitation, ainsi que des données sur votre utilisation de notre application.
+
+Utilisation des données personnelles 
+
+Nous utilisons vos données personnelles pour vous fournir nos services, tels que la gestion de stock, les ressources humaines et la comptabilité. Nous pouvons également utiliser vos données pour vous envoyer des notifications relatives à votre compte, des informations sur nos services ou des offres promotionnelles.
+
+Protection des données personnelles 
+
+Nous avons mis en place des mesures de sécurité techniques et organisationnelles pour protéger vos données personnelles contre tout accès non autorisé, perte, modification ou destruction. Nous utilisons des technologies de cryptage pour sécuriser la transmission de vos données personnelles.
+
+Partage des données personnelles 
+
+Nous ne vendons ni ne louons vos données personnelles à des tiers. Nous ne partageons vos données personnelles qu’avec des tiers si cela est nécessaire pour fournir nos services, tels que nos fournisseurs de services d’hébergement ou de stockage de données.
+
+Vos droits 
+
+Vous avez le droit d’accéder, de corriger, de supprimer ou de limiter l’utilisation de vos données personnelles. Vous pouvez également vous opposer à l’utilisation de vos données personnelles à des fins de marketing direct. Pour exercer vos droits, veuillez nous contacter à l’adresse e-mail contact@edoutech.com
+
+Modifications de la politique de confidentialité 
+
+Nous nous réservons le droit de modifier cette politique de confidentialité à tout moment. Les modifications prendront effet immédiatement après leur publication sur notre application.
+
+Nous contacter 
+
+Si vous avez des questions ou des préoccupations concernant notre politique de confidentialité ou la manière dont nous traitons vos données personnelles, veuillez nous contacter à l’adresse e-mail suivante : contact@edoutech.com
+
+Transfert de données personnelles 
+
+Si vous utilisez notre application dans un pays différent de celui dans lequel vous résidez, vos données personnelles peuvent être transférées en dehors de votre pays de résidence. Veuillez noter que les lois sur la protection des données peuvent différer selon les pays. Nous prendrons les mesures appropriées pour garantir la sécurité de vos données personnelles lors de leur transfert.
+
+Durée de conservation des données personnelles 
+
+Nous ne conservons vos données personnelles que le temps nécessaire pour vous fournir nos services ou pour respecter les exigences légales en matière de conservation de données. Après cette période, vos données personnelles seront supprimées de nos systèmes.
+
+ Contenu du site
+
+Toutes les marques, photographies, textes, commentaires, illustrations, images animées ou non, séquences
+vidéo, sons, ainsi que toutes les applications informatiques qui pourraient être utilisées pour faire fonctionner ce site et plus généralement tous les éléments reproduits ou utilisés sur le site sont protégés par les lois en vigueur au titre de la propriété intellectuelle.
+Ils sont la propriété pleine et entière de l'éditeur ou de ses partenaires. Toute reproduction, représentation, utilisation ou adaptation, sous quelque forme que ce soit, de tout ou partie de ces éléments, y compris les applications informatiques, sans l'accord préalable et écrit de l'éditeur, sont strictement interdites. Le fait pour l'éditeur de ne pas engager de procédure dès la prise de connaissance de ces utilisations non autorisées ne vaut pas acceptation desdites utilisations et renonciation aux poursuites.
+
+Gestion du site
+
+Pour la bonne gestion du site, l'éditeur pourra à tout moment :
+
+- suspendre, interrompre ou limiter l'accès à tout ou partie du site, réserver l'accès au site, ou à certaines parties du site, à une catégorie déterminée d'internautes ;
+- supprimer toute information pouvant en perturber le fonctionnement ou entrant en contravention avec les lois nationales ou internationales ;
+- suspendre le site afin de procéder à des mises à jour.
+
+Utilisation de cookies 
+
+Nous utilisons des cookies pour améliorer votre expérience utilisateur sur notre application. Les cookies sont de petits fichiers texte qui sont stockés sur votre appareil lorsque vous visitez notre application. Ils nous permettent de vous reconnaître lors de votre prochaine visite et de personnaliser notre application en fonction de vos préférences. Vous pouvez désactiver l’utilisation de cookies dans les paramètres de votre navigateur.
+
+Responsabilités
+
+Le matériel de connexion au site que vous utilisez est sous votre entière responsabilité. Vous devez prendre toutes les mesures appropriées pour protéger votre matériel et vos propres données notamment d'attaques virales par Internet. 
+
+- du fait du non-respect par vous des présentes conditions générales.
+L'éditeur n'est pas responsable des dommages causés à vous-même, à des tiers et/ou à votre équipement du fait de votre connexion ou de votre utilisation du site et vous renoncez à toute action contre lui de ce fait.
+Si l'éditeur venait à faire l'objet d'une procédure amiable ou judiciaire en raison de votre utilisation du site, il pourra se retourner contre vous pour obtenir l'indemnisation de tous les préjudices, sommes, condamnations et frais qui pourraient découler de cette procédure.
+
+Liens hypertextes
+
+La mise en place par les utilisateurs de tous liens hypertextes vers tout ou partie du site est autorisée par l'éditeur. Tout lien devra être retiré sur simple demande de l'éditeur.
+Toute information accessible via un lien vers d'autres sites n'est pas publiée par l'éditeur. L'éditeur ne dispose d'aucun droit sur le contenu présent dans ledit lien.
+
+
+Enfants 
+
+Notre application n’est pas destinée aux enfants de moins de 16 ans. Nous ne collectons pas sciemment de données personnelles auprès de personnes de moins de 16 ans. Si nous avons connaissance de la collecte de données personnelles auprès d’un enfant de moins de 16 ans, nous prendrons les mesures nécessaires pour supprimer ces données de nos systèmes.
+
+Photographies et représentation des produits
+Les photographies de produits, accompagnant leur description, ne sont pas contractuelles et n'engagent pas l'éditeur.
+
+Loi applicable**
+
+Les présentes conditions d'utilisation du site sont régies par la loi française et soumises à la compétence des
+tribunaux du siège social de l'éditeur, sous réserve d'une attribution de compétence spécifique découlant d'un texte de loi ou réglementaire particulier.
+
+Conclusion 
+
+Nous sommes engagés à protéger vos données personnelles et à respecter votre vie privée. Si vous avez des questions ou des préoccupations concernant notre politique de confidentialité ou la manière dont nous traitons vos données personnelles, n’hésitez pas à nous contacter à l’adresse e-mail contact@edoutech.com.
+    </div>
+</template>
+<script>
+export default {
+    auth: false,
+    layout: 'landing',
+  
+      mounted(){
+      },
+
+}
+</script>
